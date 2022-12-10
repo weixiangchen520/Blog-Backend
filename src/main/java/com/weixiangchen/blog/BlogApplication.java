13d15543d7811b5c103858067401fd87faf4d12c
weixiangchen520 <1981468862@qq.com>
2022-12-11 02:18:54 +0800

    feat: init

---
+++ src/main/java/com/weixiangchen/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.weixiangchen.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
